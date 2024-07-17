16474466ba6b5b10c33aa7d88bd42ea50d17a012
Praneeth Reddy <118764946+praneeth-75@users.noreply.github.com>
2024-07-17 11:52:41 +0530

    Add files via upload

---
+++ PersonalPortfolio/src/main/java/com/task1/PersonalPortfolio/PersonalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.task1.PersonalPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPortfolioApplication.class, args);
+	}
+
+}
